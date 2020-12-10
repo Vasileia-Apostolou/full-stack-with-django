a086295c4f62645803f249950ad264c85761be6c
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-12-10 22:56:53 +0000

    Changes

--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from .models import Item
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'items' : items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
@@ -16,7 +16,7 @@ def add_item(request):
     if request.method == 'POST':
         name = request.POST.get('item_name')
         done = 'done' in request.POST
-    Item.objects.create(name=name, done=done)
+        Item.objects.create(name=name, done=done)
 
         return redirect('get_todo_list')
     return render(request, 'todo/add_item.html')
